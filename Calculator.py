b3bcb0e96e94c7d3a40e89c2c2c2c5ab8cdcb74e
Simon Muriu <muriusimon6@gmail.com>
2025-03-12 13:00:18 +0300

    Initial commit

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+def Calculate(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        return num1 / num2
+    else:
+        return "Invalid operator"
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operator = input("Enter operator (+, -, *, /): ")
+
+print(Calculate(num1, num2, operator)) 
